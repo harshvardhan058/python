5152343a97435ae1e8274b882e5f6199d612a0b6
Harshvardhan Makwana <harshvardhanmakwana786@gmail.com>
2024-02-02 11:31:05 +0530

    Setup

---
+++ chai.py
@@ -0,0 +1,3 @@
+from hello_python import chai
+
+chai("JUST Import Checking nothing new Just enjoy")
---
+++ hello_python.py
@@ -0,0 +1,9 @@
+print("Hello Python Just Checking")
+
+def chai(n):
+    print(n);
+
+chai("Just pythoning")
+
+test_1 = "Final test"
+test_2 = "Second test"
